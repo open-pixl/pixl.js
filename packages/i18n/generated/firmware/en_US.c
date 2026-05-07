944aa2c55d233b8c5d32ba1d7cdfa8def55873fe
Alexandre Vaz <alexandre@vaz.srv.br>
2026-05-06 22:37:59 -0300

    fix: keep LCD firmware within memory budget

--- packages/i18n/generated/firmware/en_US.c
+++ packages/i18n/generated/firmware/en_US.c
@@ -1,13 +1,13 @@
 #include "string_id.h"
 
 const char *lang_en_US[_L_COUNT] = {
-    [_L_APP_SETTINGS_DISPLAY_FLIP] = "Flip Screen",
-    [_L_APP_SETTINGS_RETURN_KEY] = "Return Button",
-    [_L_APP_SETTINGS_RETURN_KEY_DETECT] = "Detect Return Button",
-    [_L_APP_SETTINGS_RETURN_KEY_DETECT_FAILED] = "Cannot start return detect",
-    [_L_APP_SETTINGS_RETURN_KEY_DETECT_PROMPT] = "Press physical Return button",
-    [_L_APP_SETTINGS_RETURN_KEY_DETECT_RUNNING] = "Detection already running",
-    [_L_APP_SETTINGS_RETURN_KEY_NOT_CONFIGURED] = "Not configured",
-    [_L_APP_SETTINGS_RETURN_KEY_NOT_DETECTED] = "Return button not detected",
-    [_L_APP_SETTINGS_RETURN_KEY_SET] = "Return button set: GPIO %u",
+    [_L_APP_SETTINGS_DISPLAY_FLIP] = "Flip",
+    [_L_APP_SETTINGS_RETURN_KEY] = "Return Key",
+    [_L_APP_SETTINGS_RETURN_KEY_DETECT] = "Detect Key",
+    [_L_APP_SETTINGS_RETURN_KEY_DETECT_FAILED] = "Detect failed",
+    [_L_APP_SETTINGS_RETURN_KEY_DETECT_PROMPT] = "Press return key",
+    [_L_APP_SETTINGS_RETURN_KEY_DETECT_RUNNING] = "Detect running",
+    [_L_APP_SETTINGS_RETURN_KEY_NOT_CONFIGURED] = "Unset",
+    [_L_APP_SETTINGS_RETURN_KEY_NOT_DETECTED] = "Key not found",
+    [_L_APP_SETTINGS_RETURN_KEY_SET] = "Key GPIO %u",
 };
